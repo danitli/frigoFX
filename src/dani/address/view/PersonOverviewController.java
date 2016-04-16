d9afb5f2a4020acb202364559c5aa6dffbd85329
danitli <dnannis@gmail.com>
2016-04-16 13:05:26 -0300

    Funciono la comunicacion entre javaFX y el servicio rest para obtener y guardar una tropa

--- src/dani/address/view/PersonOverviewController.java
+++ src/dani/address/view/PersonOverviewController.java
@@ -1,14 +1,37 @@
 package dani.address.view;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Date;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import javax.net.ssl.HttpsURLConnection;
+
+import com.google.common.reflect.TypeToken;
+import com.google.gson.Gson;
+
 import dani.address.MainApp;
 import dani.address.model.Person;
+import dani.address.model.bean.especie.EspecieBean;
+import dani.address.model.bean.tropa.TropaBean;
 import dani.address.util.DateUtil;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.concurrent.Task;
+import javafx.concurrent.WorkerStateEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.ComboBox;
 
 public class PersonOverviewController {
 	
@@ -31,9 +54,18 @@ public class PersonOverviewController {
     private Label cityLabel;
     @FXML
     private Label birthdayLabel;
+    
+    @FXML
+    private ComboBox especie;
 
     // Reference to the main application.
     private MainApp mainApp;
+    
+	private ObservableList<EspecieBean> especieList;
+	private static final String JSON_URL = "http://localhost:8080/frigorifico/rest/especies";
+	private static final String JSON_URL_GUARDAR_TROPA  = "http://localhost:8080/frigorifico/rest/nueva_tropa_en_palco";
+	private final ExecutorService executorService = Executors.newCachedThreadPool();
+	private TropaBean tropaBean = new TropaBean();
 
     /**
      * The constructor.
@@ -148,6 +180,172 @@ public class PersonOverviewController {
 
         	alert.showAndWait();
         }
+    }
+    
+    
+    @FXML
+    private void handleGuardarTropa() {
+        EspecieBean selectedEspecieBean = (EspecieBean) especie.getSelectionModel().getSelectedItem();
+        
+        if (selectedEspecieBean != null) {
+        	
+        	tropaBean.setEspecieId(selectedEspecieBean.getIdEspecie());
+        	tropaBean.setAnimalesRecibidos(140);
+        	tropaBean.setEstablecimientoId(1);
+        	tropaBean.setProcendeciaId(1);
+        	
+        	System.out.println(tropaBean);
+        	
+        	executorService.submit(guardarTropaBean);
+        	guardarTropaBean.setOnSucceeded(new EventHandler<WorkerStateEvent>() {
+    			@Override
+    			public void handle(WorkerStateEvent t) {
+    				guardarTropaBean.getValue();
+    				System.out.println(tropaBean);
+    			}
+    		});
+
+
+        } else {
+            // Nothing selected.
+           	Alert alert = new Alert(AlertType.ERROR);
+        	alert.setTitle("Error Dialog");
+        	alert.setHeaderText("No selecciono especie");
+        	alert.setContentText("Por favor seleccione una especie valida");
+
+        	alert.showAndWait();
+        }
+    }
+    
+    
+   
+    /**
+	 * Read the URL and return the json data
+	 * 
+	 * @param urlString
+	 * @return
+	 * @throws Exception
+	 */
+	private static String readUrl(String urlString) throws Exception {
+		BufferedReader reader = null;
+		try {
+			URL url = new URL(urlString);
+			reader = new BufferedReader(new InputStreamReader(url.openStream()));
+			StringBuffer buffer = new StringBuffer();
+			int read;
+			char[] chars = new char[1024];
+			while ((read = reader.read(chars)) != -1)
+				buffer.append(chars, 0, read);
+
+			return buffer.toString();
+		} finally {
+			if (reader != null)
+				reader.close();
+		}
+	}
+	
+	// HTTP POST request
+		private String writeUrl(String url) throws Exception {
+
+			URL obj = new URL(url);
+			HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+			//add reuqest header
+			con.setRequestMethod("POST");
+			con.setRequestProperty("Content-Type", "application/json");
+			//con.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
+
+			//String urlParameters = "sn=C02G8416DRJM&cn=&locale=&caller=&num=12345";
+			
+			// Send post request
+			con.setDoOutput(true);			
+			DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+			//wr.writeBytes(urlParameters);
+			wr.writeChars(tropaBean.toString());
+			wr.flush();
+			wr.close();
+
+			int responseCode = con.getResponseCode();
+			//System.out.println("\nSending 'POST' request to URL : " + url);
+			//System.out.println("Post parameters : " + urlParameters);
+			System.out.println("Response Code : " + responseCode);
+
+			BufferedReader reader = null;
+			try {
+				reader = new BufferedReader(new InputStreamReader(con.getInputStream()));
+				StringBuffer buffer = new StringBuffer();
+				int read;
+				char[] chars = new char[1024];
+				while ((read = reader.read(chars)) != -1)
+					buffer.append(chars, 0, read);
+				
+				String tropa = buffer.toString();
+				System.out.println(tropa);
+				return tropa;
+				
+			} finally {
+				if (reader != null)
+					reader.close();
+			}
+			
+		}
+
+	
+    
+    /**
+	 * Task to fetch details from JSONURL
+	 * 
+	 * @param <V>
+	 */
+	private Task<List<EspecieBean>> fetchList = new Task<List<EspecieBean>>() {
+		@Override
+		protected List<EspecieBean> call() throws Exception {
+			List<EspecieBean> list = null;
+			try {
+				Gson gson = new Gson();
+				list = new Gson().fromJson(readUrl(JSON_URL), new TypeToken<List<EspecieBean>>() {
+				}.getType());
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			return list;
+		}
+	};
+	
+    /**
+	 * Task to fetch details from JSONURL
+	 * 
+	 * @param <V>
+	 */
+	private Task<TropaBean> guardarTropaBean = new Task<TropaBean>() {
+		@Override
+		protected TropaBean call() throws Exception {
+			
+			try {
+				Gson gson = new Gson();
+				tropaBean = new Gson().fromJson(writeUrl(JSON_URL_GUARDAR_TROPA), new TypeToken<TropaBean>() {
+				}.getType());
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			return tropaBean;
+		}
+	};
+    
+    @FXML
+    private void cargarComboEspecie(){
+    	executorService.submit(fetchList);
+    	fetchList.setOnSucceeded(new EventHandler<WorkerStateEvent>() {
+			@Override
+			public void handle(WorkerStateEvent t) {
+				especieList = FXCollections.observableArrayList(fetchList.getValue());
+				System.out.println(especieList);
+				
+				especie.setItems(especieList);
+				
+			}
+		});
+
     }
 
 }
